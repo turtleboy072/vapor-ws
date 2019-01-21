b7fd3f9ff7ade3a13d28295d9bfc18b70497e01c
Jim Allan <jimallan@MACs-MacBook-Pro-2.local>
2019-01-21 17:01:45 +0530

    zzzz

---
+++ .build/x86_64-apple-macosx10.10/debug/CNIOLinux.build/shim.c.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/jimallan/Downloads/starter/location-track-server/.build/checkouts/swift-nio.git-6105345694714423636/Sources/CNIOLinux/shim.c
---
+++ .build/x86_64-apple-macosx10.10/debug/CNIOOpenSSL.build/empty.c.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/jimallan/Downloads/starter/location-track-server/.build/checkouts/swift-nio-ssl.git--1045596292306961330/Sources/CNIOOpenSSL/empty.c
